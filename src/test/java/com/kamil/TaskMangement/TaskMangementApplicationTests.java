27fe2b53b31c1ffaf0e142717179f0b8d9f2719e
kamilxcv <kamil.sz00@proton.me>
2026-08-14 12:57:16 +0200

    task management foundation

---
+++ src/test/java/com/kamil/TaskMangement/TaskMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamil.TaskMangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
