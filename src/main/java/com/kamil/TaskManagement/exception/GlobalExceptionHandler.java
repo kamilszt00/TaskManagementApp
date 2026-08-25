71487b15e9dd234c71c8f246d3805dadef94b69b
kamilxcv <kamil.sz00@proton.me>
2026-08-25 13:58:05 +0200

    Add global DataIntegrityViolationException handling with structured ErrorResponse

--- src/main/java/com/kamil/TaskManagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kamil/TaskManagement/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package com.kamil.TaskManagement.exception;
 import com.kamil.TaskManagement.DTO.ErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.*;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,5 +26,15 @@ public class GlobalExceptionHandler {
                 .build();
         return ResponseEntity.status(errorResponse.getStatus())
                 .body(errorResponse);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> dataIntViolation(DataIntegrityViolationException dataIntegrityViolationException) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message("One or more referenced entities do not exist, or this operation violates a data constraint.")
+                .status(400)
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 }
