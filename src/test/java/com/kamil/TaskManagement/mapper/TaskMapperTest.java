646e9c3bda93e6495e42c074aa873d2efecdff96
kamilxcv <kamil.sz00@proton.me>
2026-08-26 21:06:31 +0200

    unit testing for Task enhanced with AssertJ

--- src/test/java/com/kamil/TaskManagement/mapper/TaskMapperTest.java
+++ src/test/java/com/kamil/TaskManagement/mapper/TaskMapperTest.java
@@ -1,20 +1,14 @@
 package com.kamil.TaskManagement.mapper;
 
 import com.kamil.TaskManagement.DTO.CreateTaskRequest;
-import com.kamil.TaskManagement.DTO.TaskResponse;
 import com.kamil.TaskManagement.model.Task;
-import com.kamil.TaskManagement.repository.ProjectRepository;
-import com.kamil.TaskManagement.repository.TagRepository;
-import com.kamil.TaskManagement.repository.TaskRepository;
-import com.kamil.TaskManagement.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
 import org.junit.jupiter.api.*;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 class TaskMapperTest {
@@ -27,20 +21,14 @@ class TaskMapperTest {
         CreateTaskRequest dto = CreateTaskRequest.builder()
                 .title("The title")
                 .status("TODO")
-                .DueDate(LocalDateTime.now().plusDays(33))
-                .projectID(1)
-                .assigneeID(1)
-                .tagsID(new HashSet<>(Arrays.asList(1,2,3)))
+                .dueDate(LocalDateTime.now().plusDays(33))
                 .build();
 
         Task task = mapper.toRequest(dto);
-        assertEquals(dto.getTitle(),task.getTitle());
-        assertEquals(dto.getStatus(),task.getStatus());
-        assertNotNull(task.getDueDate());
-        assertEquals(dto.getDueDate(),task.getDueDate());
-        assertNull(task.getProject());
-        assertNull(task.getUser());
-        assertTrue(task.getTags() == null || task.getTags().isEmpty());
+        assertThat(task)
+                .usingRecursiveComparison()
+                .ignoringFields("project","user","tags","id")
+                .isEqualTo(dto);
     }
 
 }
